dde7990935c5f32aecae32bb63edbe8ab78ce0ec
kheller18 <keenanaheller@gmail.com>
2023-01-25 14:31:20 -0800

    added problem description

--- problems/leetcode/smaller-numbers-than-current.js
+++ problems/leetcode/smaller-numbers-than-current.js
@@ -1,7 +1,10 @@
 /**
  * @param {number[]} nums
  * @return {number[]}
+ * Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+ * That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
  */
+
 var smallerNumbersThanCurrent = function(nums) {
   let map = new Map();
   let sorted = nums.slice().sort((a, b) => a - b)
