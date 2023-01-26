bf2e464a58b2aa3e8f91977b4d09beaa332765a7
kheller18 <keenanaheller@gmail.com>
2023-01-25 17:33:26 -0800

    updated smaller numbers than current syntax

--- problems/leetcode/smaller-numbers-than-current.js
+++ problems/leetcode/smaller-numbers-than-current.js
@@ -7,16 +7,16 @@
 
 var smallerNumbersThanCurrent = function(nums) {
   let map = new Map();
-  let sorted = nums.slice().sort((a, b) => a - b)
+  let sorted = nums.slice().sort((a, b) => a - b);
   sorted.forEach((number, idx) => map.has(number) ? null : map.set(number, idx));
-  return nums.map((num) => map.get(num))
+  return nums.map((num) => map.get(num));
 };
 
 let output = smallerNumbersThanCurrent([8,1,2,2,3]);
 let expectedOutput = [4,0,1,1,3];
 
 if (output === expectedOutput) {
-  console.log(`Success! ${output} matches ${expectedOutput}.`)
+  console.log(`Success! ${output} matches ${expectedOutput}.`);
 } else {
   console.log(`Unsuccessful. Expected output is ${expectedOutput}.`);
 }
