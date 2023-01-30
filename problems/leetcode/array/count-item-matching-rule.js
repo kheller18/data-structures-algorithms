c3ca91ac4f39965a1ac4411c1f4992abcc2c050f
kheller18 <keenanaheller@gmail.com>
2023-01-30 15:34:12 -0800

    updated function for better runtime and memory

--- problems/leetcode/array/count-item-matching-rule.js
+++ problems/leetcode/array/count-item-matching-rule.js
@@ -9,31 +9,13 @@
  * ruleKey == "color" and ruleValue == colori.
  * ruleKey == "name" and ruleValue == namei.
  * Return the number of items that match the given rule.
- * RUNTIME: 89 ms (beats 46.22%)
- * MEMORY: 45.8 MB (beats 70.7%)
+ * RUNTIME: 80 ms (beats 73.48%)
+ * MEMORY: 45.6 MB (beats 82.22%)
  */
 var countMatches = function(items, ruleKey, ruleValue) {
-  let count = 0;
+  const map = {'type': 0, 'color': 1, 'name': 2};
 
-  items.forEach((item) => {
-      switch (ruleKey) {
-          case 'type':
-              item[0] === ruleValue ? count++ : null;
-              break;
-
-          case 'color':
-              item[1] === ruleValue ? count++ : null;
-              break;
-
-          case 'name':
-              item[2] === ruleValue ? count++ : null;
-              break;
-
-          default: null;
-      }
-  });
-
-  return count;
+  return items.reduce((accumulator, item) => item[map[ruleKey]] === ruleValue ? accumulator += 1 : accumulator, 0);
 };
 
 const output = countMatches([["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], "color", "silver");
