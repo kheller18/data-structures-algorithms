fc348479d472f08db1eaa1560d0dd6fd846538d4
kheller18 <keenanaheller@gmail.com>
2023-01-29 17:08:16 -0800

    updated syntax

--- problems/leetcode/array/final-value-var-after-operations.js
+++ problems/leetcode/array/final-value-var-after-operations.js
@@ -4,13 +4,13 @@
  * There is a programming language with only four operations and one variable X:
  * ++X and X++ increments the value of the variable X by 1.
  * --X and X-- decrements the value of the variable X by 1.
- * RUNTIME: 81 ms (beats 38.37%)
- * MEMORY: 42.2 MB (beats 81.97%)
+ * RUNTIME: 80 ms (beats 40.53%)
+ * MEMORY: 42.5 MB (beats 46.59%)
  */
 var finalValueAfterOperations = function(operations) {
   let X = 0;
   operations.forEach((oper) => {
-      (oper === 'X++' || oper === '++X') ? X++ : X--
+      (oper[1] === '+') ? X++ : X--
   })
   return X;
 };
