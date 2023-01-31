2ba1ab39d00123ec0298ada610da76ef4ec13ed6
kheller18 <keenanaheller@gmail.com>
2023-01-30 20:41:38 -0800

    updated truncate problem

--- problems/leetcode/array/truncate-sentence.js
+++ problems/leetcode/array/truncate-sentence.js
@@ -5,6 +5,8 @@
  * A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
  * For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
  * You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+ * RUNTIME: 66 ms (beats 67.16%)
+ * MEMORY: 42.2 MB (beats 45.91%)
  */
 const truncateSentence = function(s, k) {
   return s.split(' ').slice(0, k).join(' ');
